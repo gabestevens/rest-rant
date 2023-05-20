3bdc86fad3a1aac13039ee0be73b14f37cf6f2b0
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-05-20 01:54:44 -0400

    changes

--- models/places.js
+++ models/places.js
@@ -1,13 +1,16 @@
-module.exports = [{
-    name: 'Sabaidee Thai & Sushi Bar',
-    city: 'concord',
-    state: 'NC',
-    cuisines: 'Thai, Pan-Asian, sushi',
-    pic: '/images/thai.jpeg'
-  }, {
-    name: 'Mac Tabby Cat Cafe',
-    city: 'charlotte',
-    state: 'NC',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cat-cafe.jpeg'
-  }]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: { type:String },
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: { type: Number }
+})
+
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
+module.exports = mongoose.model('place', placeSchema)
