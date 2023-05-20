7ee29ec0c4baace6b7879d59b8a49fbca742b843
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-05-20 01:20:13 -0400

    new place view

---
+++ Views/places/Views/places/show.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function Show () {
+    return(
+        <Def>
+            <main>
+                <h2>details page for one place</h2>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = Show
