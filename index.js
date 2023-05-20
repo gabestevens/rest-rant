e03b00f899d66eafe54992103afb65c7b55cf033
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-05-20 00:47:32 -0400

    added home page view

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/places', require('./controllers/places'))
+
 
 app.get('/',(req,res)=>{
-    res.send('hello world!')
+    res.render('home')
 })
 
-app.listen(8080)
+app.get('*', (req,res)=>{
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+app.listen(process.env.PORT)
