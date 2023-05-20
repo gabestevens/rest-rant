523c73966e31718c3790f00371d5bdfa66d02f02
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-05-20 00:56:01 -0400

    added other page views

---
+++ Views/places/Views/places/Views/places/showPage.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function showPage () {
+    return(
+        <Def>
+            <main>
+                <h2>details page for one place</h2>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = showPage
---
+++ Views/places/Views/places/newPage.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function newPage () {
+    return(
+        <Def>
+            <main>
+                <h2>new page create form</h2>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = newPage
