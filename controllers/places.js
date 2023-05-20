201c83910903f4a00e336bcbaa592f085eefb237
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-05-20 01:24:28 -0400

    changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,33 @@
 //
 const router = require('express').Router()
 
+let places = [{
+  name: 'Sabaidee Thai & Sushi Bar',
+  city: 'concord',
+  state: 'NC',
+  cuisines: 'Thai, Pan-Asian, sushi',
+  pic: '/images/thai.jpeg'
+}, {
+  name: 'Mac Tabby Cat Cafe',
+  city: 'charlotte',
+  state: 'NC',
+  cuisines: 'Coffee, Bakery',
+  pic: '/images/cat-cafe.jpeg'
+}]
+
 //GET /places
 router.get('/', (req,res) => {
-    let places = [{
-        name: 'Sabaidee Thai & Sushi Bar',
-        city: 'concord',
-        state: 'NC',
-        cuisines: 'Thai, Pan-Asian, sushi',
-        pic: '/images/thai.jpeg'
-      }, {
-        name: 'Mac Tabby Cat Cafe',
-        city: 'charlotte',
-        state: 'NC',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-cafe.jpeg'
-      }]
-      
+
     res.render('places/index', { places })
 })
 
 router.get('/new', (req,res)=>{
-    res.render('places/new')
+    res.render('./new')
+})
+router.post('/',(req,res)=>{
+  console.log(req.body)
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 // router.get('/places/:id',(req,res)=>{
